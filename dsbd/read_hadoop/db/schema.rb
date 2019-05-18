4e2a69b89b83f15d1b5a5f9d26d35e2b94570ce8
Caio Henrique <caiohenriqueads@gmail.com>
2019-05-18 17:55:32 -0300

    include boostrap gem and dependencies and run migrations pedding

--- dsbd/read_hadoop/db/schema.rb
+++ dsbd/read_hadoop/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_185644) do
+ActiveRecord::Schema.define(version: 2019_05_18_190241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "file_inputs", force: :cascade do |t|
+    t.string "name"
+    t.string "local"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_file_inputs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_05_18_185644) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "file_inputs", "users"
 end
