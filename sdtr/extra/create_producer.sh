28ef4b1c8d5fd58c6b2f35b495feae00a1ee7010
chvs <caiohenriqueads@gmail.com>
2019-06-18 20:37:18 -0300

    Update create_producer.sh

--- sdtr/extra/create_producer.sh
+++ sdtr/extra/create_producer.sh
@@ -1,1 +1,2 @@
-sudo /opt/kafka/kafka_2.10-0.10.0.1/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic topic_created
+# run this on kafka/bin
+sudo ./kafka-console-producer.sh --broker-list localhost:9092 --topic topic_created
